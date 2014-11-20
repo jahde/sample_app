be0f9bc4852e1899532178e021b5870a18a5771c
Jahde <jahde@me.com>
2014-11-19 23:45:11 -0500

    Added update user to app

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,15 @@
 class UsersController < ApplicationController
-  
+
   def show
     @user = User.find(params[:id])
   end
-  
+
   def new
     @user = User.new
   end
-  
+
   def create
-    @user = User.new(user_params)      
+    @user = User.new(user_params)
     if @user.save
       sign_in @user
       flash[:success] = "Welcome to the Sample App!"
@@ -18,9 +18,22 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-  
-  private 
-  
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # Handle a successful update.
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
