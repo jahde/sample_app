3c30e9a449ff50c08fec1e100c5081eae0bc851c
Jahde <jahde@me.com>
2014-11-30 13:49:45 -0500

    Added tests for the default scope ordering and destroy micropost section

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -88,39 +88,64 @@ describe User do
     it { should_not be_valid }
   end
 
-    describe "when password doesn't match confirmation" do
-      before { @user.password_confirmation = "mismatch" }
-      it {should_not be_valid}
+  describe "when password doesn't match confirmation" do
+    before { @user.password_confirmation = "mismatch" }
+    it {should_not be_valid}
+  end
+
+  describe "with a password that's too short" do
+    before { @user.password = @user.password_confirmation = "a" * 5 }
+    it { should be_invalid }
+  end
+
+  describe "return value of authenticate method" do
+    before { @user.save! }
+    let(:found_user) { User.find_by(email: @user.email) }
+
+    describe "with valid password" do
+      it { should eq found_user.authenticate(@user.password) }
     end
 
-    describe "with a password that's too short" do
-      before { @user.password = @user.password_confirmation = "a" * 5 }
-      it { should be_invalid }
+    describe "with invalid password" do
+      let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+      it { should_not eq user_for_invalid_password }
+      specify { expect(user_for_invalid_password).to be_false }
+    end
+  end
+
+  describe "email address with mixed case" do
+    let(:mixed_case_email)  { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
+
+  describe "micropost associations" do
+
+    before { @user.save }
+    let!(:older_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+    end
+    let!(:newer_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
     end
 
-    describe "return value of authenticate method" do
-      before { @user.save! }
-      let(:found_user) { User.find_by(email: @user.email) }
+    it "should have the right microposts in the right order" do
+      expect(@user.microposts.to_a).to eq [newer_micropost, older_micropost]
+    end
 
-      describe "with valid password" do
-        it { should eq found_user.authenticate(@user.password) }
+    it "should destroy associated microposts" do
+      microposts = @user.microposts.to_a
+      @user.destroy
+      expect(microposts).not_to be_empty
+      microposts.each do |micropost|
+        expect(Micropost.where(id: micropost.id)).to be_empty
       end
-
-      describe "with invalid password" do
-        let(:user_for_invalid_password) { found_user.authenticate("invalid") }
-
-        it { should_not eq user_for_invalid_password }
-        specify { expect(user_for_invalid_password).to be_false }
-      end
     end
-    describe "email address with mixed case" do
-      let(:mixed_case_email)  { "Foo@ExAMPle.CoM" }
-
-      it "should be saved as all lower-case" do
-        @user.email = mixed_case_email
-        @user.save
-        expect(@user.reload.email).to eq mixed_case_email.downcase
-      end
-    end
+  end
 end
 
