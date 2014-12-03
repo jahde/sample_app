c4bf558d369b37ce61cd92532cac46ead23412aa
Jahde <jahde@me.com>
2014-12-03 17:05:45 -0500

    added all tests

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,23 +1,27 @@
 class RelationshipsController < ApplicationController
   before_filter :signed_in_user
 
+  respond_to :html, :js
+
   def create
     @user = User.find(params[:relationship][:followed_id])
     current_user.follow!(@user)
-    respond_to do |format|
-      format.html { redirect_to @user }
-      format.js
-    end
+    respond_with @user #respond_to :html, :js takes care of the below for you
+    # respond_to do |format|
+    #   format.html { redirect_to @user }
+    #   format.js
+    # end
     #redirect_to @user
   end
 
   def destroy
     @user = Relationship.find(params[:id]).followed
     current_user.unfollow!(@user)
-    respond_to do |format|
-      format.html { redirect_to @user }
-      format.js
-    end
+    respond_with @user #respond_to :html, :js takes care of the below for you
+    # respond_to do |format|
+    #   format.html { redirect_to @user }
+    #   format.js
+    # end
     #redirect_to @user
   end
 end
